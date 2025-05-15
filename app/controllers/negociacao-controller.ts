62d41c1bb076ac3acaf98c228dde0d92e9c2b165
rogeriop <pinheiroxrogerio@gmail.com>
2025-05-15 15:49:52 -0300

    Curso de TypeScript parte 2: avançando na linguagem -03. Visibilidade de métodos e Enumeration

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,24 +1,38 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+import { MensagemView } from '../views/mensagem-view.js';
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
+
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes);
     }
-    adiciona(): void {
+    public adiciona(): void {
         const negociacao = this.criaNegociacao();
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
+            return;
+        }
         this.negociacoes.adiciona(negociacao);
-        console.log(this.negociacoes.lista());
         this.limparFormulario();
-    
+        this.atualizaView();
     }
-    criaNegociacao(): Negociacao {
+
+    private ehDiaUtil(data: Date) {
+        return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
+    }
+    private criaNegociacao(): Negociacao {
         const negociacao = new Negociacao(
             new Date(this.inputData.value.replace(/-/g, ',')),
             parseInt(this.inputQuantidade.value),
@@ -26,10 +40,14 @@ export class NegociacaoController {
         )
         return negociacao;
     }
-    limparFormulario(): void {
+    private limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+    private atualizaView(): void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso')
     }
 }
---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,36 @@
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
+
+export class NegociacoesView extends View<Negociacoes> {
+
+    
+    protected template(model: Negociacoes): string {
+        return`
+        <table class="tabel table-hover table-borederd">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${model.lista().map(negociacao => {
+                    return `
+                        <tr>
+                            <td>${this.formatar(negociacao.data)}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        </tr>`
+                }).join('')}
+            </tbody>
+                `;
+    }
+
+    private formatar(data: Date): string {
+        return new Intl.DateTimeFormat().format(data)
+    }
+
+}
